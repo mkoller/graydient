ffa024c84ca031f35cf174eeca24101138ce7df5
Miles <mileskoller@gmail.com>
2016-01-21 11:28:08 -0600

    header

--- templates/header.php
+++ templates/header.php
@@ -22,7 +22,7 @@
                 'depth'             => 2,
                 'container'         => 'div',
                 'container_class'   => 'toggle navbar-toggle',
-        'container_id'      => 'js-bootstrap-offcanvas',
+                'container_id'      => 'js-bootstrap-offcanvas',
                 'menu_class'        => 'nav navbar-nav',
                 'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                 'walker'            => new wp_bootstrap_navwalker())
