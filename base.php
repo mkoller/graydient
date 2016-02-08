b44179e5fce201b35ee56ddf6f21bbd3d24cd989
Miles <mileskoller@gmail.com>
2016-02-08 16:21:09 -0600

    cleanup function url

--- base.php
+++ base.php
@@ -22,7 +22,7 @@ use Roots\Sage\Wrapper;
 
         <div class="content row">
                    <div class="hero">
-              <img src="http://localhost:8888/wordpress/wp-content/uploads/2016/02/blog-header.png" alt="">
+              <img src="<?php echo get_site_url(); ?>/wp-content/uploads/2016/02/blog-header.png" alt="">
                   <div class="services-header">
                       <h1><?php the_field('hero_header'); ?></h1>
                       <p> Graydient Creative is a full spectrum marketing solutions company for businesses in the hospitality and service industries. We’re more than just pretty websites and social media posts. We were born with creativity, and raised on innovation. </p>
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 
 <header class="main-header">
-            <div class="logo"><a href="/"><img src="http://localhost:8888/wordpress/wp-content/uploads/2016/02/graydient-logo.png" /></a></div>
+            <div class="logo"><a href="/"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2016/02/graydient-logo.png" /></a></div>
             <div class="menu-link">
                 <span class="bracket-left">[</span>
                 <div class="bar"></div>
